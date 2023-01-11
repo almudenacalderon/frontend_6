c3aafd0a97b83d3f0703faa0a1a6ae00a754cbf1
Almudena Calderon <1797322@alu.murciaeduca.es>
2023-01-11 10:20:23 +0100

    Modificacion admin.js y creacion migrations.js

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -2,13 +2,15 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from 'components/react-admin/users';
 import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
+import { MigrationList } from 'components/react-admin/migrations';
 
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
+import MigrationIcon from '@mui/icons-material/Storage';
 
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider('http://encuentro.test/api/records');
 
 const RAdmin = () => (
   <Admin
@@ -16,6 +18,7 @@ const RAdmin = () => (
     dataProvider={dataProvider}
     layout={AdminLayout}
   >
+    <Resource name="migrations" list={MigrationList} icon={MigrationIcon} recordRepresentation="migration" />
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
